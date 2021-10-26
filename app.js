7c9908eb72b4d171adad6f2243860df81d1d05ee
Alessandro Giuzio <giuzio@icloud.com>
2021-10-26 17:20:47 +0200

    JS added

---
+++ app.js
@@ -0,0 +1,12 @@
+const buttons = document.getElementsByClassName('dd');
+const btn = document.getElementById('fas');
+const email = document.getElementById('email');
+
+
+btn.onclick = function () {
+    if (email === @){
+
+    }
+
+  console.log('clicked');
+};
